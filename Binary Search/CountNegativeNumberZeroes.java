578b10599547708c99518e9bc004e0423d67652e
TheRedCoderX <Rjha60571@gmail.com>
2025-01-25 16:13:15 +0530

    Create CountNegativeNumberZeroes.java

---
+++ Binary Search/CountNegativeNumberZeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        int i = 0;
+        int j = m - 1;
+
+        while (i < n && j >= 0) {
+            if (grid[i][j] < 0) {
+                count += (n - i); 
+                j--; 
+            } else {
+                i++; 
+            }
+        }
+        return count;
+    }
+}
