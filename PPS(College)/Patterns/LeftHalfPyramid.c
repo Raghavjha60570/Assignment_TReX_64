06c9373062590edb4bd513e5cfb728027c96ee17
TReX <Rjha60571@gmail.com>
2025-03-04 11:53:35 +0530

    Update LeftHalfPyramid.c

--- PPS(College)/Patterns/LeftHalfPyramid.c
+++ PPS(College)/Patterns/LeftHalfPyramid.c
@@ -41,4 +41,22 @@ void alphabetRightHalf(int n){
      }
 }
 
+// Additional Code For Adding Left hand Pyramid  
+#include <stdio.h>
+
+int main() {
+     int n =4 ;
+     for(int i=0 ;i<n ; i++){
+         for(int j=0;j<2*(n-i) ;j++){
+             printf(" ");
+         }
+         for(int k=0 ;k<=i; k++){
+             printf("* ");
+         }
+         printf("\n");
+     }
+     
+   
+}
+
 
