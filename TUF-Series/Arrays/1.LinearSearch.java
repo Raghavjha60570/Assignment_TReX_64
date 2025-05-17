bd89139d1d406f97a6c957c17c5d8232398a5789
TReX <Rjha60571@gmail.com>
2025-05-17 10:35:13 +0530

    Create 1.LinearSearch.java

---
+++ TUF-Series/Arrays/1.LinearSearch.java
@@ -0,0 +1,13 @@
+
+//My solution is basically the most common approach for linear search
+class Solution {
+    public int linearSearch(int nums[], int target) {
+		//Your code goes here
+       for ( int i=0 ; i<nums.length ;i++){
+        if(nums[i]==target){
+          return i;
+        }
+       }
+       return -1;
+    }
+}
