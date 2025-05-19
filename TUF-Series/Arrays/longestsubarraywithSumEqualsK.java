a79e616be4e0cb827ecffd1bc7c4fd500cc07303
TReX <Rjha60571@gmail.com>
2025-05-19 11:40:58 +0530

    Create longestsubarraywithSumEqualsK.java

---
+++ TUF-Series/Arrays/longestsubarraywithSumEqualsK.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestSubarray(int[] nums, int k) {
+        int maxLen = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+}
